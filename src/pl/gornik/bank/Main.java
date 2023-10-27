a26d3b50cbcd5b8a523c3b281ae86da822da898c
lkoin <lkoin.kontakt@gmail.com>
2023-10-27 08:44:12 +0200

    dadsa

--- src/pl/gornik/bank/Main.java
+++ src/pl/gornik/bank/Main.java
@@ -1,9 +1,11 @@
 package pl.gornik.bank;
 
 import java.time.LocalTime;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         BankAccount bankAccount = new BankAccount("12345678901234567890123456",5000);
         System.out.println(bankAccount);
 
@@ -19,21 +21,40 @@ public class Main {
         bankAccount.payCheck(1000);
         System.out.println("Saldo po wyplacie: " +bankAccount.getAccountBalance());
 
-        int timeDeposit = 2;
+        int timeDeposit = 1;
         double amount = 2000;
         double termPercent= 10;
         LocalTime startTime = LocalTime.now();
+//        System.out.println(startTime);
+        LocalTime endTime = startTime.plusMinutes(timeDeposit);
+//        System.out.println(endTime);
+        LocalTime timeNow = LocalTime.now();
 
         //zalozenie lokaty
         TermDeposit termDeposit = new TermDeposit(timeDeposit,amount,termPercent,startTime);
         //odjecie z konta
         bankAccount.setAccountBalance(bankAccount.getAccountBalance()-amount);
+
+        String end ="";
+        while (!end.equals("stop")) {
+            System.out.printf("Poczekaj %d i potem wpisz stop: \n",timeDeposit);
+            end = scanner.nextLine();
+        }
+
+
+        if (timeNow.isBefore(endTime)) {
+            System.out.println("Lokata trwa");
+        }
+        else {
+            bankAccount.setAccountBalance(
+                    bankAccount.getAccountBalance()+ termDeposit.calculateProfit());
+        }
+
         //wypisanie stanu po wplaceniu na lokate
         System.out.printf("Stan konta po załozeniu lokaty %.2f",bankAccount.getAccountBalance());
         System.out.println("");
         //obliczenie lokaty
-        bankAccount.setAccountBalance(
-        bankAccount.getAccountBalance()+ termDeposit.calculateProfit());
+
         //wypisanie profitu z lokaty
         System.out.printf("Saldo z lokatą terminowa: %.2f",bankAccount.getAccountBalance());
     }
